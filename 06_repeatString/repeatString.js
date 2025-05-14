335dccfde2870ebb783b77f5f8753f13a4981364
Learning Code <learningcode62@gmail.com>
2025-05-14 13:33:11 +0930

    done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatAmount) {
+    let repeated =  "";
+    if(repeatAmount < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < repeatAmount; i++){
+        repeated += word;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
